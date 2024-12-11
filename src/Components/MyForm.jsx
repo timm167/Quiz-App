26468052d4694a261cfbaa729ad8ad8293fc48a9
Tim Charteris <tim.charterii@gmail.com>
2024-12-11 15:07:23 +0000

    set random difficulty and topic for incomplete form

--- src/Components/MyForm.jsx
+++ src/Components/MyForm.jsx
@@ -1,3 +1,20 @@
+import React from 'react';
+import { useState } from 'react';
+
+const topics = [Sport, Celebrities, Animals]
+const difficulties = [Easy, Medium, Hard]   
+
+function setInitialTopic() {
+    return topics[Math.floor(Math.random()*topics.length)] // returns a random topic
+}
+
+function setInitialDifficulty() {
+    return difficulties[Math.floor(Math.random()*difficulties.length)] // returns a random difficulty
+}
+
 export default function MyForm() {
+    const [topic, setTopic] = useState(setInitialTopic());
+    const [difficulty, setDifficulty] = useState(setInitialDifficulty());
+
     return 'Form here'
 }
