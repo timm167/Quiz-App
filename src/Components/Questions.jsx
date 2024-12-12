e09f0572f13b3b5308fc5b6b7eb742e68cc1fe89
Tim Charteris <tim.charterii@gmail.com>
2024-12-12 13:35:29 +0000

    display list of questions

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-export default function Questions({questionData}) {
+export default function Questions({questionData}){
     return (
-    <div>
-        <p>{JSON.stringify(questionData)}</p>
-    </div>
-    )
+        questionData.results.map((question, index) => {
+        const options = [...question.incorrect_answers, question.correct_answer]
+        options.sort(() => Math.random() - 0.5);
+        return (
+            <div key={index}>
+                <h2>{question.question}</h2>
+                <form>
+                    {options.map((option, i) => (
+                        <label key={i}>
+                            <input type="radio" name="question" value={option}/>
+                            {option}
+                        </label>
+                    ))}
+                </form>
+            </div>
+        );
+    }));
 }
