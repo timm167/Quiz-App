4d4f8438b81c0ed4edaf904be751e6455e6f2207
Tim Charteris <tim.charterii@gmail.com>
2024-12-12 13:46:05 +0000

    display questions one at a time

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import {useState} from 'react';
 
 export default function Questions({questionData}){
+    const [currentQuestion, setCurrentQuestion] = useState(0); // state for displaying current question
+
+    const question = questionData.results[currentQuestion]; // gets the current question from the questionData prop
+    const options = [...question.incorrect_answers, question.correct_answer] // creates an array of all possible answers
+    options.sort(() => Math.random() - 0.5); // shuffles the array of answers
+
     return (
-        questionData.results.map((question, index) => {
-        const options = [...question.incorrect_answers, question.correct_answer]
-        options.sort(() => Math.random() - 0.5);
-        return (
-            <div key={index}>
-                <h2>{question.question}</h2>
-                <form>
-                    {options.map((option, i) => (
-                        <label key={i}>
-                            <input type="radio" name="question" value={option}/>
-                            {option}
-                        </label>
-                    ))}
-                </form>
-            </div>
-        );
-    }));
-}
+        <div id="question-container">
+            <h2>{question.question}</h2>
+            <form>
+                {options.map((option, i) => (
+                    <label key={i}>
+                        <input type="radio" name="question" value={option}/>
+                        {option}
+                    </label>
+                ))}
+            </form>
+            {/* <div>
+                <button onClick={handlePreviousQuestion}>
+                    Previous
+                </button>
+                <button onClick={handleNextQuestion}>
+                    Next
+                </button>
+            </div> */}
+        </div>
+    );
+};
